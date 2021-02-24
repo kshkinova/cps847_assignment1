a585304436e940bd0a77c42c5d508856605a4726
Brett <beetan87@hotmail.com>
2021-02-24 18:10:23 -0500

    Updated question echo and added weather checking for city input.

--- A1-bot.py
+++ A1-bot.py
@@ -9,6 +9,9 @@ import requests
 # Handles events from Slack
 from slackeventsapi import SlackEventAdapter
 
+# Handle NLP for city recognition
+import spacy
+
 # Load the Token from .env file
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -21,35 +24,8 @@ slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/eve
 # Using WebClient in slack, there are other clients built-in as well !!
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
-# connect the bot to the channel in Slack Channel
-#client.chat_postMessage(channel='#cps-847-course', text='Send Message Demo')
-
 # Get Bot ID
 BOT_ID = client.api_call("auth.test")['user_id']
-
-# Modify to perform weather API request
-# url='https://geek-jokes.sameerkumar.website/api'
-# @app.route('/joke', methods=['GET','POST'])
-# def joke():
-#     data = request.form
-#     channel_id = data.get('channel_id')
-#     response = requests.request("GET", url)
-#     print(response.text)
-#     client.chat_postMessage(channel=channel_id, text=response.text)
-#     return Response(), 200
-
-# handling Message Events
-# @slack_event_adapter.on('message')
-# def message(payload):
-#     print(payload)
-#     event = payload.get('event',{})
-#     user_id = event.get('user')
-#     channel_id = event.get('channel')
-#     if BOT_ID != user_id:
-#         text = event.get('text')
-#         if text.endswith("?"):
-#             resp = "Good question. " + text.capitalize()
-#             client.chat_postMessage(channel=channel_id, text=resp)
 
 # handling mention Events and echoing questions
 @slack_event_adapter.on('app_mention')
@@ -59,11 +35,36 @@ def message(payload):
     user_id = event.get('user')
     channel_id = event.get('channel')
     if BOT_ID != user_id:
-        text = event.get('text')
+        text = event.get('text').replace("<@U01NXKV0F2S>", "").strip().capitalize() # strip the mention tag
         if text.endswith("?"):
-            resp = "You talking to me? " + text.capitalize().replace("<@u01nxkv0f2s>", "")
-            print(resp)
+            client.chat_postMessage(channel=channel_id, text=text)
+        elif is_city_only(text):
+            weather = get_weather(text)
+            resp = "Current weather in {} {}".format(text, weather)
             client.chat_postMessage(channel=channel_id, text=resp)
+
+
+
+nlp = spacy.load('en_core_web_lg')
+def is_city_only(text):
+    if len(text.split()) != 1:
+        return False
+    
+    doc = nlp(text)
+    return False if not doc.ents else True
+
+weather_api = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+def get_weather(city):
+    url = weather_api.format(city, os.environ['WAPI_KEY'])
+    response = requests.request("GET", url)
+    if response.ok:
+        json = response.json()
+        # print(json)
+        resp_str = "feels like " + str(round(json['main']['feels_like'] - 273.15,2)) + " degrees celcius with " + json['weather'][0]['description'] + "."
+        # print(resp_str)
+        return resp_str
+    
+    return "is unknown."
 
 
 
